047a519639205de4ad134104a66cc7ffe1d52dbe
Stas <follooowgod@gmail.com>
2023-12-20 23:56:40 +0900

    add cycle&&if

--- 1/script.js
+++ 1/script.js
@@ -21,15 +21,65 @@
 
 // alert(`Привет, ${user}`);
 
-console.log('arr' + " - object");
+// console.log('arr' + " - object");
 
-console.log( 4 + " - object");
+// console.log( 4 + " - object");
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
-// incr++;
-// decr--;
+// // incr++;
+// // decr--;
 
-console.log(++incr);
-console.log(--decr);
+// console.log(++incr);
+// console.log(--decr);
+
+if (4==9) {
+    console.log('ok!');
+} else {
+    console.log('error');
+}
+
+
+if (num<49) {
+    console.log('Error');
+} else if (num>100) {
+    console.log('Mnogo');
+} else {
+    console.log('ok!');
+}
+
+
+(num ===50) ? console.log('ok!') : console.log('Error');
+
+
+switch (num) {
+    case 49:
+        console.log('Nexerno');
+        break;
+        case 49: 
+    }
+    
+    
+    
+    while (num <= 55) {
+        console.log(num);
+        num++;
+    }
+    
+    let num = 50;
+    do{
+        console.log(num);
+        num++
+    }
+    while (num<55);
+
+    for (let i = 1; i<15; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+    
+        console.log(i);
+
+    }
